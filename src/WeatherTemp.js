378ff5a5b795d2c8c2f5f7b672702dca6d0c3f47
Astar05 <nhuynh78@gmail.com>
2023-12-30 21:51:34 -0500

    remove unnecessary elements

--- src/WeatherTemp.js
+++ src/WeatherTemp.js
@@ -14,10 +14,6 @@ export default function WeatherTemp(props) {
     setUnit("fahrenheit");
   }
 
-  function celsiusToFahrenheit(celsius) {
-    return (celsius * 9) / 5 + 32;
-  }
-
   function fahrenheitToCelsius(fahrenheit) {
     return ((fahrenheit - 32) * 5) / 9;
   }
